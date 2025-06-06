ff518d03bc48f536f5190b5acc075d174138decf
chirtu <chitufatuma45@gmail.com>
2025-06-06 20:36:37 +0300

    DCP started

--- dataclub/core/urls.py
+++ dataclub/core/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 
 urlpatterns = [
 
-    path('',views.home, name='home')
+    path('',views.home, name='home'),
+    # path('events/', views.events, name='events'),
+    # path('resources/', views.resources, name='resources'),
+    # path('members/', views.members, name='members'),
+    # path('contacts/', views.contact, name='contacts'),
+    
     
 ]
