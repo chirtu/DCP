da9706b5cc6039c0509a4307d2933027451e75c8
chirtu <chitufatuma45@gmail.com>
2025-06-10 19:59:04 +0300

    some changes

--- dataclub/customadmin/urls.py
+++ dataclub/customadmin/urls.py
@@ -5,5 +5,10 @@ urlpatterns = [
 
     path('', views.admin_login, name='login'),
     path('dashboard/', views.admin_home, name='home'),
-   
+    
+    path('dashboard/', views.admin_home, name='home'),
+    path('abouts/', views.admin_abouts, name='abouts'),
+    path('members/', views.admin_members, name='members'),
+    path('events/', views.admin_events, name='events'),
+    path('resources/', views.admin_resources, name='resources'),
 ]
--- dataclub/customadmin/views.py
+++ dataclub/customadmin/views.py
@@ -13,3 +13,14 @@ def admin_login(request):
 def admin_home(request):
     return render(request, 'customadmin/home.html')
 
+def admin_events(request):
+    return render(request, 'customadmin/pages/events.html')
+
+def admin_abouts(request):
+    return render(request, 'customadmin/pages/about.html')
+
+def admin_resources(request):
+    return render(request, 'customadmin/pages/resources.html')
+
+def admin_members(request):
+    return render(request, 'customadmin/pages/members.html')
